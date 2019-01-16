928dfa6d03e6791a1b73aa7ef54a6e09c4ff8f8c
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-01-17 01:23:05 +0800

    Update main_ric.py

--- getFileResult.py
+++ getFileResult.py
@@ -5,8 +5,9 @@ for m in [2, 3]:
     model_name = "mngic" * (m == 1) + "mhdic" * (m == 2) + "mric" * (m == 3) + "_pps"
     for pps in [1, 2, 3]:
         for wpiwp in [bool(0), bool(1)]:
-            for data_setting in [2]:
-                data_set_name = "email_directed" * (data_setting == 1) + "email_undirected" * (data_setting == 2) + "WikiVote_directed" * (data_setting == 3)
+            for data_setting in [2, 4]:
+                data_set_name = "email_directed" * (data_setting == 1) + "email_undirected" * (data_setting == 2) + \
+                                "WikiVote_directed" * (data_setting == 3) + "NetPHY_undirected" * (data_setting == 4)
                 for prod_setting in [1, 2]:
                     for prod_setting2 in [1, 2, 3]:
                         product_name = "r1p3n" + str(prod_setting) + "a" * (prod_setting2 == 2) + "b" * (prod_setting2 == 3)
